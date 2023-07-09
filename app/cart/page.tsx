4b38138887d1e54fc951599406e74f9e318f215a
pallab99 <kitkatpallab@gmail.com>
2023-07-09 12:09:33 +0600

    added the cart view

---
+++ app/cart/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+
+import Navbar from '@/Components/Navbar';
+import Cart from '../../Components/Cart';
+
+export default function Page() {
+  return <>
+    <Navbar />
+    <Cart />
+  </>;
+}
